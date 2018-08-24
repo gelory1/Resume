2ac9232f51edd1c6ec93de67761ddf0f81229f62
gelory <917589697@qq.com>
2018-08-24 18:04:07 +0800

    加入网易云、小程序、vue多人共享博客项目

--- js/jobs.js
+++ js/jobs.js
@@ -6,33 +6,45 @@
         barPro.className = "bar-inner1"
         canvasImg.classList.remove('hid');
         bookmarksImg.classList.remove('hid');
-        bannerImg.classList.remove('hid');
-        document.querySelector('#bannerImg img').classList.remove('active');
+        musicImg.classList.remove('hid');
+        vueClientImg.classList.remove('hid');
+        weiAppImg.classList.remove('hid');
+        document.querySelector('#musicImg img').classList.remove('active');
         document.querySelector('#canvasImg img').classList.remove('active');
     }
     pro2.onclick = function () {
         barPro.className = "bar-inner2"
         canvasImg.classList.add('hid');
         bookmarksImg.classList.add('hid');
-        bannerImg.classList.remove('hid');
+        musicImg.classList.remove('hid');
+        vueClientImg.classList.remove('hid');
+        weiAppImg.classList.add('hid');
         document.querySelector('#canvasImg img').classList.remove('active');
-        document.querySelector('#bannerImg img').classList.add('active');
+        document.querySelector('#musicImg img').classList.add('active');
     }
     pro3.onclick = function () {
         barPro.className = "bar-inner3"
         canvasImg.classList.remove('hid');
         bookmarksImg.classList.remove('hid');
-        bannerImg.classList.add('hid');
-        document.querySelector('#bannerImg img').classList.remove('active');
+        musicImg.classList.add('hid');
+        vueClientImg.classList.add('hid');
+        weiAppImg.classList.remove('hid');
+        document.querySelector('#musicImg img').classList.remove('active');
         document.querySelector('#canvasImg img').classList.add('active');
     }
-    document.querySelector('#bannerImg img').onclick = function () {
-        window.open("https://gelory1.github.io/myBanner/index2.html", "_blank")
+    document.querySelector('#musicImg img').onclick = function () {
+        window.open("https://gelory1.github.io/music-demo/src/", "_blank")
     }
     document.querySelector('#bookmarksImg img').onclick = function () {
         window.open("https://gelory1.github.io/myBookmarks/index.html", "_blank")
     }
     document.querySelector('#canvasImg img').onclick = function () {
         window.open("https://gelory1.github.io/myCanvas/index.html", "_blank")
+    }
+    document.querySelector('#vueClientImg img').onclick = function () {
+        window.open("https://gelory1.github.io/blog-client-view/", "_blank")
+    }
+    document.querySelector('#weiAppImg img').onclick = function () {
+        window.open("http://pdheymdhu.bkt.clouddn.com/%E4%BA%8C%E7%BB%B4%E7%A0%81.png", "_blank")
     }
 }.call();
